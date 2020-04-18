31882befc37159b18886efce5061c3ecfbb99929
MusicGivesMeLife <4193175+MusicGivesMeLife@users.noreply.github.com>
2020-04-18 16:40:03 -0400

    Added Metar request

--- TestWeatherRest.py
+++ TestWeatherRest.py
@@ -1,9 +1,15 @@
-import urllib3
+import urllib3 json
 
 headers = {
   'token': 'ChlVriwCTmwJd2XMLZ2UwY1uovz2QbhoSqdlg9_wWnk'
 }
 http = urllib3.PoolManager()
 request = http.request('GET', 'https://avwx.rest/api/station/kgfk', fields=headers)
+request2 = http.request('GET', 'https://avwx.rest/api/metar/kgfk', fields=headers)
 
 print(request.data)
+with open('Info.txt', 'w') as outfile1:
+    json.dump(request.data, outfile1)
+print(request2.data)
+with open('Metar.txt', 'w') as outfile2:
+    json.dump(request2.data, outfile2)
