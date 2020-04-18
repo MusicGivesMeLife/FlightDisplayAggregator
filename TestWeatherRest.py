3ae79b89321e9f8b50112e66d3a817d0f7b95a45
MusicGivesMeLife <4193175+MusicGivesMeLife@users.noreply.github.com>
2020-04-18 17:02:24 -0400

    Added TAF report

--- TestWeatherRest.py
+++ TestWeatherRest.py
@@ -2,15 +2,17 @@ import urllib3
 import json
 
 headers = {
-  'token': 'ChlVriwCTmwJd2XMLZ2UwY1uovz2QbhoSqdlg9_wWnk'
+    'Content-Type':'application/json'
+    'token': 'ChlVriwCTmwJd2XMLZ2UwY1uovz2QbhoSqdlg9_wWnk'
 }
 http = urllib3.PoolManager()
 request = http.request('GET', 'https://avwx.rest/api/station/kgfk', fields=headers)
 request2 = http.request('GET', 'https://avwx.rest/api/metar/kgfk', fields=headers)
+request3 = http.request('GET', 'https://avwx.rest/api/taf/kgfk', fields=headers)
 
-print(request.data)
-with open('Info.txt', 'w') as outfile1:
-    json.dump(str(request.data), outfile1)
-print(request2.data)
-with open('Metar.txt', 'w') as outfile2:
-    json.dump(str(request2.data), outfile2)
+with open('Info.json', 'wb') as outfile1:
+    outfile1.write(request.data)
+with open('Metar.json', 'wb') as outfile2:
+    outfile2.write(request2.data)
+with open('TAF.json', 'wb') as outfile3:
+    outfile3.write(request3.data)
